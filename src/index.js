767fe6e1db20600960af72678554140d48fec658
AsheToll <github email address>
2019-08-07 17:39:44 -0400

    DONE. AND WE DID EXTRA CREDIT

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    let breedArray = [];
 
     fetch(imgUrl)
     .then(resp => resp.json())
@@ -32,13 +33,35 @@ document.addEventListener('DOMContentLoaded', function(){
             let ulBreeds = document.getElementById('dog-breeds')
             let li = document.createElement('li')
             ulBreeds.appendChild(li)
-            li.innerText = key 
+            breedArray.push(li.innerText = key)
             li.addEventListener('click', function() {
                 li.style.color = "blue"
             })
         }
 
         
+    })
+    const sortButton = document.getElementById('sortButton');
+    sortButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        let li = document.querySelectorAll('li')
+        let selected = document.querySelector('#breed-dropdown')
+        let array = []
+        breedArray.forEach(li => {
+            if (li[0] === selected.value) {
+                array.push(li)
+            }
+        } )
+        let ulBreeds = document.getElementById('dog-breeds')
+        ulBreeds.innerText = "";
+        array.forEach(breed => {
+            let filteredLi = document.createElement('li');
+            filteredLi.innerText = breed;
+            ulBreeds.appendChild(filteredLi);
+            filteredLi.addEventListener('click', function() {
+                filteredLi.style.color = "blue"
+            })
+        })
     })
 
 
