6db44b79a49666c7aa006c88fafb85ca1f883645
Trevahr Robertson <Trevahr@gmail.com>
2019-08-07 16:27:57 -0400

    handoff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(data => {
+
+        
+        
+        data.message.forEach(dog =>{
+            let container = document.getElementById('dog-image-container')
+            let div = document.createElement('div')
+            container.appendChild(div)
+            let dogImg = document.createElement('img')
+            div.appendChild(dogImg).src = dog
+        } )
+      
+    } )
+
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(data => {
+        // debugger
+
+        for (const key in data.message){
+            let ulBreeds = document.getElementById('dog-breeds')
+            let li = document.createElement('li')
+            ulBreeds.appendChild(li)
+            li.innerText = key 
+            li.addEventListener('click', function() {
+                li.style.color = "blue"
+            })
+        }
+
+        
+    })
+
+
+})
