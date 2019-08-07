50f2f38c2147f27c49992bd4ba71b461e8997667
claudiasolbes <cssolbes@hotmail.com>
2019-08-07 16:25:03 -0400

    challenges 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(imageCollection => {
+      imageCollection.message.forEach((dogImage) => {
+      let img = document.createElement("img")
+      img.src = dogImage
+      let dogList = document.getElementById("dog-image-container")
+      let dogLi = document.createElement("div")
+      dogLi.appendChild(img)
+      dogList.appendChild(dogLi)
+      // debugger
+    })
+    })
+
+  fetch("https://dog.ceo/api/breeds/list/all")
+     .then(resp => resp.json())
+     .then(breeds => {
+      let breedObject = breeds.message
+      let breedList = document.getElementById("dog-breeds")
+      for(const breedName in breedObject){
+        let breedLi = document.createElement("li")
+        breedLi.innerText = breedName
+        breedLi.id = breedName
+        breedList.appendChild(breedLi)
+        breedLi.addEventListener("click", function(e){
+
+          debugger
+        })
+      }
+     })
+})
