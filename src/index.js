ebd124bd5b298563d82385f7fd9efa64c5020955
lisantwi <lisaantwi@gmail.com>
2019-08-07 16:07:04 -0400

    challenge 1&2 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(data =>{ data.message.forEach((image)=>{
+        let container = document.getElementById("dog-image-container")
+        let dogDiv = document.createElement("div")
+        let dogImg = document.createElement("img")
+        container.appendChild(dogDiv)
+        dogDiv.appendChild(dogImg)
+        dogImg.src = image
+    })
+    })
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+        for (const key in data.message){
+
+            let breedUl = document.getElementById("dog-breeds")
+            let breedLi = document.createElement("li")
+            breedUl.appendChild(breedLi)
+            breedLi.innerText = key
+
+        }
+       
+    
+    })
+ 
+    
+
+})
+
+
